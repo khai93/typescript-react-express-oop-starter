acc27fbeed4d9cc6dd96d33c402f311a4d544a8f
khai93 <33293519+khai93@users.noreply.github.com>
2020-10-30 10:57:49 -0500

    fix bug

--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -3,10 +3,6 @@ import { Logger } from "../logger/logger";
 import { RouteUtility } from "../utils/routes";
 import { join, resolve } from 'path';
 import { readdir } from 'fs/promises';
-<<<<<<< HEAD
-=======
-import { WeatherController } from "./weather/weatherController";
->>>>>>> 0b534e81131d0f3866b99782f30f365e52d55e4c
 import { ControllerUtility } from "../utils/controllers";
 
 const logger = new Logger();
@@ -20,10 +16,5 @@ const defaultControllerDependencies = {
 };
 
 export default [
-<<<<<<< HEAD
     new StatusController(defaultControllerDependencies)
-=======
-    new StatusController(defaultControllerDependencies),
-    new WeatherController(defaultControllerDependencies)
->>>>>>> 0b534e81131d0f3866b99782f30f365e52d55e4c
 ]
